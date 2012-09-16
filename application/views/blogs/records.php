53199c744ca605f205981d17cda50a328848abf6
Pattanai Kawinvasin <Jerusalem@Pattanais-MacBook-Pro.local>
2012-09-16 10:46:35 +0700

    HTML

--- application/views/blogs/records.php
+++ application/views/blogs/records.php
@@ -5,6 +5,7 @@
 	</head>
 	<body>
 		
+		<p><a href="<?php echo site_url('blogs/add'); ?>">Add new entry</a></p>
 		
 		<table cellpading="2" cellspacing="2" border="1">
 			<thead>
@@ -13,17 +14,26 @@
 					<th>Body</th>
 					<th>Created at</th>
 					<th>Updated at</th>
+					<th>Actions</th>
 				</tr>
 			</thead>
 			<tbody>
+				<!-- Start loop entries -->
 				<?php if (count($entries)) foreach ($entries as $entry) : ?>
 				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
+					<td><?php echo $entry->title; ?></td>
+					<td><?php echo $entry->body; ?></td>
+					<td><?php echo $entry->created_at; ?></td>
+					<td>
+						<?php echo ($entry->updated_at) ? $entry->updated_at : '-' ?>
+					</td>
+					<td>
+						<a href="<?php echo site_url('blogs/edit/'.$entry['id']); ?>">Edit</a>
+						<a href="<?php echo site_url('blogs/delete/'.$entry['id']); ?>">Delete</a>
+					</td>
 				</tr>
-				<?php 
+				<?php endforeach; ?>
+				<!-- End loop entries -->
 			</tbody>
 		</table>
 		
