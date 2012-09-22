3c0661f06523476b6302f29cf820f5ca87897154
Pattanai Kawinvasin <Jerusalem@Pattanais-MacBook-Pro.local>
2012-09-22 10:56:52 +0700

    SQL

--- MySQL/Codeigniter.sql
+++ MySQL/Codeigniter.sql
@@ -1,66 +1,48 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 15, 2012 at 08:50 PM
--- Server version: 5.5.25
--- PHP Version: 5.4.4
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50525
+ Source Host           : 127.0.0.1
+ Source Database       : Codeigniter
 
+ Target Server Type    : MySQL
+ Target Server Version : 50525
+ File Encoding         : utf-8
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ Date: 09/22/2012 10:56:36 AM
+*/
 
---
--- Database: `Codeigniter`
---
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `blogs`
---
-
+-- ----------------------------
+--  Table structure for `blogs`
+-- ----------------------------
+DROP TABLE IF EXISTS `blogs`;
 CREATE TABLE `blogs` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) DEFAULT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
   `title` varchar(255) NOT NULL,
   `body` longtext NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
+  `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `blogs`
---
-
-INSERT INTO `blogs` (`id`, `user_id`, `title`, `body`, `created_at`, `updated_at`) VALUES
-(5, 0, 'This is my first entry', 'Something', '2012-09-15 16:50:36', NULL),
-(6, 0, 'sadasdsa', 'dasdsad', '2012-09-15 16:52:37', NULL),
-(7, 0, 'asdas', 'dasdasd', '2012-09-15 16:52:48', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
