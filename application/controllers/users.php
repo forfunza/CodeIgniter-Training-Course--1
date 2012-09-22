6d63024b65746794757ccfc78fa52aa279306f80
Pattanai Kawinvasin <Jerusalem@Pattanais-MacBook-Pro.local>
2012-09-22 11:05:08 +0700

    ok

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -33,7 +33,25 @@ class Users extends CI_Controller {
 			
 			if ($this->form_validation->run() === true)
 			{
-				// load model and process command				
+				$user = new User();
+				$user->email    = $this->input->post('email', true);
+				$user->password = $this->input->post('password', true);		
+				$user->save();
+				
+				$users_id = $user->id;				
+				
+				// if valid all processes
+				if ($users_id !== false)
+				{
+					// set flash data					
+					$message = 'Yeah Let\'s Play.';
+					$this->session->set_flashdata('success', $message);
+					
+					// redirect application to listing
+					redirect('auth/login#added-id-'.$users_id);					
+				}
+				// error something I don't know
+				show_error('Error unknown');
 			}
 			else
 			{
